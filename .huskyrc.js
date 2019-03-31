d46b1ea60387f1c7832ac01c5d8a7ca826d2c23e
omoikane09 <omoikane09@gmail.com>
2019-03-31 16:14:23 +0900

    huskyrc 에 cross-env 적용

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,13 +1,6 @@
-const os = require('os');
-
-let command = '';
-if (os.type() === 'Windows_NT') {
-  command = 'set ';
-}
-
 // .huskyrc
 module.exports = {
   hooks: {
-    'pre-commit': `${command}NODE_ENV=production && lint-staged`
+    'pre-commit': `cross-env NODE_ENV=production && lint-staged`
   }
 };
