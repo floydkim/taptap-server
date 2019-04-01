5b16695d7aa6e2bec66b9f0e537aaac13a5313da
floydkim <r2d2em@empal.com>
2019-04-01 21:52:59 +0900

    get-coupons-count API 완료
    요청, 응답 내용은 API 문서 참조 바랍니다.

--- controller/stores/coupons/getCouponsCount.js
+++ controller/stores/coupons/getCouponsCount.js
@@ -3,7 +3,7 @@ const { getCouponsCount } = require('../../../models/coupons');
 exports.getCouponsCount = (request, response) => {
   getCouponsCount(request.body)
     .then(count => {
-      response.status(200).json(count);
+      response.status(200).json({ count });
     })
     .catch(error => {
       console.error(error);
