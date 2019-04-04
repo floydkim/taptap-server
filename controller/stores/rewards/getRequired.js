e7750f30c7759e4cb369fe36e7228cf4689a4e85
floydkim <r2d2em@empal.com>
2019-04-04 11:11:18 +0900

    /stores/rewards/get-required 가 가게이름도 반환하도록 변경
    storeID를 받는 get-required API가 Stores테이블에서 해당 가게 이름도 받아 리턴하도록 수정.
    이를 위해 models/stores/getName 모델 추가.

--- controller/stores/rewards/getRequired.js
+++ controller/stores/rewards/getRequired.js
@@ -1,9 +1,18 @@
 const { getRequired } = require('../../../models/rewards');
+const { getName } = require('../../../models/stores');
 
 exports.getRequired = (request, response) => {
   getRequired(request.body) // 목표 개수 REQUIRED 받아옴
     .then(requiredCount => {
-      response.status(200).json({ REQUIRED: requiredCount });
+      getName(request.body)
+        .then(name => {
+          response
+            .status(200)
+            .json({ REQUIRED: requiredCount, storeName: name });
+        })
+        .catch(error => {
+          console.log(error);
+        });
     })
     .catch(error => {
       console.log(error);
